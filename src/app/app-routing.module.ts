03184cff20a9e16dfc97a8d1e6d0864354709ac9
Sahil Makhijani <svmakhijani94@gmail.com>
2021-05-01 22:01:33 -0700

    Added dedication route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
   { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
   { path: 'history', loadChildren: () => import('./history/history.module').then(m => m.HistoryModule) },
+  { path: 'dedication', loadChildren: () => import('./dedication/dedication.module').then(m => m.DedicationModule) },
   { path: '**', redirectTo: '/' }
 ];
 
